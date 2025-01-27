b65e65060094874b8515429ad2e75a94676991b4
Tommy-BOU <tommy.boubaker@free.fr>
2025-01-27 10:46:52 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <img src="public/world.svg" alt="" />
+    </>
+  )
+}
+
+export default App
