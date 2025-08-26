e3047fef5454cbc0c9fe372ffc4b719fa4a54cae
Tommy-BOU <tommy.boubaker@free.fr>
2025-08-26 19:03:15 +0200

    Add fields query to API call to avoid error 400

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   const fetchData = async () => {
     setLoading(true);
     try {
-      const res = await fetch(`https://restcountries.com/v3.1/all`);
+      const res = await fetch(`https://restcountries.com/v3.1/all?fields=name,capital,cca2,cca3,cca3_eh,region,population,flags,continents`);
       if (!res.ok) {
         throw new Error("Failed to fetch data");
       }
